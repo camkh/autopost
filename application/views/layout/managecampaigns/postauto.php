8e827a9a5532096e756f7ba063864f0cb52217ea
s <s@gmail.com>
2019-03-31 23:22:41 +0700

    ff

--- application/views/layout/managecampaigns/postauto.php
+++ application/views/layout/managecampaigns/postauto.php
@@ -59,8 +59,9 @@ if(!empty($bloglinkA[0])) {
     $createNewBlog = true;
     $bNewName = generateRandomString(1).'1';
 }
+$blogLinkID = !empty($this->input->get('blog_link_id')) ? $this->input->get('blog_link_id') : @$bLinkID;
 $btemplate = "D:&bsol;&bsol;PROGRAM&bsol;&bsol;templates&bsol;&bsol;";
-$blogPostID = ($this->input->get('action') =='generate') ? $staticdata->blogid : $this->input->get('blog_link_id');
+$blogPostID = ($this->input->get('action') =='generate') ? $staticdata->blogid : $blogLinkID;
 ?>
 <code id="codeB" style="width:300px;overflow:hidden;display:none"></code>
 <code id="codeC" style="width:300px;overflow:hidden;display:none">macro=&quot;CODE:&quot;;macro+=&quot;URL GOTO=https://developers.facebook.com/tools/debug/sharing/?q=xxxxxxxxxxx\n&quot;;macro+=&quot;TAG POS=1 TYPE=SPAN ATTR=TXT:We&lt;SP&gt;can't&lt;SP&gt;review&lt;SP&gt;this&lt;SP&gt;website&lt;SP&gt;because&lt;SP&gt;the*\n&quot;;retcode=iimPlay(macro);var error=true;if(retcode&lt;0){error=false;}; if(!error){macro=&quot;CODE:&quot;;macro+=&quot;URL GOTO=&quot;+homeUrl+&quot;managecampaigns/ajax?lid=&quot;+bid+&quot;&amp;p=autopostblog\n&quot;;retcode=iimPlay(macro);};if(error){macro=&quot;CODE:&quot;;macro+=&quot;URL GOTO=&quot;+homeUrl+&quot;setting?blog_link_a=1&amp;bid=&quot;+bid+&quot;&amp;title=&amp;status=2\n&quot;;macro+=&quot;WAIT SECONDS=2\n&quot;;macro+=&quot;URL GOTO=&quot;+homeUrl+&quot;managecampaigns/autopost?startpost=1\n&quot;;retcode=iimPlay(macro);}</code>
