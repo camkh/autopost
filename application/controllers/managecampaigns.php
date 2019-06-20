623e129d69daab425de1a88446da5228a89f4043
camkh <socheatnews@hotmail.com>
2019-06-20 15:48:34 +0700

    add http://socialnews.teenee.com

--- application/controllers/managecampaigns.php
+++ application/controllers/managecampaigns.php
@@ -3095,6 +3095,48 @@ HTML;
                 $obj->site = 'site';
                 return $obj;
                 break;
+            case 'socialnews.teenee.com':
+                $content = '';
+                foreach($html->find ('#main div[itemprop=articleBody]') as $item) {
+                    $content .= $item->innertext;
+                }
+                $content = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', "", $content);
+                $content = preg_replace('/<ins\b[^>]*>(.*?)<\/ins>/is', '<div class="setAds"></div>', $content);
+
+                $regex = '/< *img[^>]*src *= *["\']?([^"\']*)/';
+                preg_match_all( $regex, $content, $matches );
+                $ImgSrc = array_pop($matches);
+                // reversing the matches array
+                if(!empty($ImgSrc)) {
+                    foreach ($ImgSrc as $image) {
+                        $imagedd = strtok($image, "?");
+                        if(!preg_match('/^(http)/', $imagedd)){
+                            $imagedd = 'http://socialnews.teenee.com/penkhao/'.$imagedd;
+                        }
+                        $file_title = basename($imagedd);
+                        $fileName = FCPATH . 'uploads/image/'.$file_title;
+                        @copy($imagedd, $fileName);   
+                        $images = $this->mod_general->uploadtoImgur($fileName);
+                        if(empty($images)) {
+                            $apiKey = '76e9b194c1bdc616d4f8bb6cf295ce51';
+                            $images = $this->Mod_general->uploadToImgbb($fileName, $apiKey);
+                            if($images) {
+                                @unlink($fileName);
+                            }
+                        } else {
+                            $gimage = @$images; 
+                            @unlink($fileName);
+                        }
+                        if(!empty($gimage)) {
+                            $content = str_replace($image,$gimage,$content);
+                        }
+                    }
+                }
+                $obj->conent = $content;
+                $obj->fromsite = $parse['host'];
+                $obj->site = 'site';
+                return $obj;
+                break;
             default:
                 $dataA = @$html->find ( '#main .post', 0 );
                 $checked = @$html->find ( '#Blog1 .youtube_link', 0 );
